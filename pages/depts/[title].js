99c2faa654f872085e13fb359e219736c10cb7a2
Ivan-M-Rigney <rigneyi@uci.edu>
2023-02-16 09:11:21 -0800

    Department pages now use accordion and dark mode.

--- pages/depts/[title].js
+++ pages/depts/[title].js
@@ -1,9 +1,9 @@
 // INDIVIDUAL DEPARTMENT PAGE - ALL COURSES IN DEPT
 
-import { GET_COURSE, SCHEDULE } from '@/backend/queries'
-import Course from '@/components/Course'
-import { useQuery } from '@apollo/client'
-import { useRouter } from 'next/router'
+import { GET_COURSE, SCHEDULE } from "@/backend/queries"
+import Course from "@/components/Course"
+import { useQuery } from "@apollo/client"
+import { useRouter } from "next/router"
 
 import {
   Button,
@@ -14,9 +14,32 @@ import {
   ListGroupItem,
   Row,
   Stack,
-} from 'react-bootstrap'
+  Accordion,
+  useAccordionButton,
+} from "react-bootstrap"
 
-import PageLayout from '../../components/PageLayout'
+import PageLayout from "../../components/PageLayout"
+
+function CustomToggle({ children, eventKey }) {
+  return (
+    <button
+      type="button"
+      style={{
+        border: "0px",
+        backgroundColor: "rgba(52, 52, 52, 0)",
+        textAlign: "left",
+        color: "rgb(189,193,197)",
+        width: "100%",
+      }}
+      onClick={useAccordionButton(eventKey)}
+      flush
+    >
+      {children}
+    </button>
+  )
+}
+
+const textWhite = "rgb(189,193,197)"
 
 const DeptPage = () => {
   const router = useRouter()
@@ -25,19 +48,19 @@ const DeptPage = () => {
   const { loading, data } = useQuery(SCHEDULE, {
     variables: {
       year: 2023,
-      quarter: 'Winter',
+      quarter: "Winter",
       department: title,
     },
-    errorPolicy: 'all',
+    errorPolicy: "all",
   })
 
   const courseTitles = new Set()
   const courseMap = new Map()
   data?.result.map((course) => {
-    courseTitles.add(course.course.title)
+    courseTitles.add(course.course?.title)
     courseMap.set(
-      course.course.title,
-      course.course.department + ' ' + course.course.number
+      course.course?.title,
+      course.course?.department + " " + course.course?.number
     )
   })
 
@@ -46,76 +69,112 @@ const DeptPage = () => {
   return (
     <PageLayout>
       <Container>
-        <h1>{title}</h1>
+        <h1 style={{ color: textWhite }}>{title}</h1>
         {loading ? (
           <p>Loading...</p>
         ) : (
-          <Stack gap={4}>
+          <Accordion defaultActiveKey="0" alwaysOpen>
             {courseArry.map((c) => (
-              <Card style={{ width: '100' }} key={c}>
-                <Card.Header>{courseMap.get(c) + ' - ' + c}</Card.Header>
-                <ListGroup
-                  horizontal
-                  style={{ width: '95%', textAlign: 'center' }}
-                >
-                  <Col xs={1}>Section</Col>
-                  <Col xs={2}>CRN</Col>
-                  <Col xs={2}>Days</Col>
-                  <Col xs={2}>Time</Col>
-                  <Col xs={2}>Building</Col>
-                  <Col xs={2}>Instructors</Col>
-                </ListGroup>
-                {data?.result.map((e) => {
-                  return e.course.title == c ? (
-                    <>
-                      <ListGroup
-                        horizontal
-                        className="list-group-flush"
-                        style={{ width: '100v' }}
-                      >
-                        {/* <ListGroup.Item>{e.course.title}</ListGroup.Item> */}
-                        <Col xs={1}>
-                          <ListGroup.Item>{e.section.type}</ListGroup.Item>
-                        </Col>
-                        <Col xs={2}>
-                          <ListGroup.Item>{e.section.code}</ListGroup.Item>
-                        </Col>
-                        <Col xs={2}>
-                          <ListGroup.Item>
-                            {e.meetings.map((m) => m.days)}
-                          </ListGroup.Item>
-                        </Col>
-                        <Col xs={2}>
-                          <ListGroup.Item>
-                            {e.meetings.map((m) => m.time)}
-                          </ListGroup.Item>
-                        </Col>
-                        <Col xs={2}>
-                          <ListGroup.Item>
-                            {e.meetings.map((m) => m.building)}
-                          </ListGroup.Item>
-                        </Col>
-                        <Col xs={'3'}>
-                          <ListGroup.Item>
-                            {e.instructors.at(0).name == null &&
-                            e.instructors.at(-1).name == null ? (
-                              <p>Not available</p>
-                            ) : (
-                              <>
-                                {e.instructors.map((instructor) => (
-                                  <p>{instructor?.name}</p>
-                                ))}
-                              </>
-                            )}
-                          </ListGroup.Item>
-                        </Col>
-                      </ListGroup>
-                    </>
-                  ) : null
-                })}
+              <Card className="course-cards">
+                <Card.Header>
+                  <CustomToggle eventKey={c}>
+                    {courseMap.get(c) + " - " + c}
+                  </CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey={c}>
+                  <Card.Body>
+                    <ListGroup
+                      horizontal
+                      style={{ width: "95%", textAlign: "center" }}
+                    >
+                      <Col style={{ color: textWhite }} xs={1}>
+                        Section
+                      </Col>
+                      <Col style={{ color: textWhite }} xs={2}>
+                        CRN
+                      </Col>
+                      <Col style={{ color: textWhite }} xs={2}>
+                        Days
+                      </Col>
+                      <Col style={{ color: textWhite }} xs={2}>
+                        Time
+                      </Col>
+                      <Col style={{ color: textWhite }} xs={2}>
+                        Building
+                      </Col>
+                      <Col style={{ color: textWhite }} xs={2}>
+                        Instructors
+                      </Col>
+                    </ListGroup>
+                    {data?.result.map((e) => {
+                      return e.course?.title == c ? (
+                        <>
+                          <ListGroup
+                            horizontal
+                            className="list-group-flush"
+                            style={{ width: "100v" }}
+                          >
+                            {/* <ListGroup.Item>{e.course.title}</ListGroup.Item> */}
+                            <Col xs={1}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.section.type}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                            <Col xs={2}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.section.code}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                            <Col xs={2}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.meetings.map((m) => m.days)}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                            <Col xs={2}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.meetings.map((m) => m.time)}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                            <Col xs={2}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.meetings.map((m) => m.building)}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                            <Col xs={"3"}>
+                              <ListGroup.Item className="list-field">
+                                <p style={{ color: textWhite }}>
+                                  {e.instructors.at(0).name == null &&
+                                  e.instructors.at(-1).name == null ? (
+                                    <p>Not available</p>
+                                  ) : (
+                                    <>
+                                      {e.instructors.map((instructor) => (
+                                        <p>{instructor?.name}</p>
+                                      ))}
+                                    </>
+                                  )}
+                                </p>
+                              </ListGroup.Item>
+                            </Col>
+                          </ListGroup>
+                        </>
+                      ) : null
+                    })}
+                  </Card.Body>
+                </Accordion.Collapse>
               </Card>
             ))}
-          </Stack>
+          </Accordion>
         )}
       </Container>
     </PageLayout>
